7bdb21a4c6ddc4691cf3497de5f0ebd1ec5e427c
Shota Akizuki <64979430+shota-akizuki@users.noreply.github.com>
2020-11-26 15:20:02 +0900

    1st Commit

---
+++ src/components/Sapmle.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Sapmle: React.FC = () => {
+  return (
+    <div>
+      <button className="button" onClick={() => console.log("ボタン")}>
+        ボタン
+      </button>
+    </div>
+  );
+};
+
+export default Sapmle;
