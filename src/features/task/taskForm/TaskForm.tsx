83abd592b242018c788bac79d8e50d64ef8ee738
Shota Akizuki <64979430+shota-akizuki@users.noreply.github.com>
2020-12-30 01:06:52 +0900

    タスクの追加機能を実装

--- src/features/task/taskForm/TaskForm.tsx
+++ src/features/task/taskForm/TaskForm.tsx
@@ -1,15 +1,23 @@
 import React from "react";
+//storeの中身を使うにはuseDispatchを使う（reduxかインポート）
+import { useDispatch } from "react-redux";
 import styles from "./TaskForm.module.scss";
 import TextField from "@material-ui/core/TextField";
 import { useForm } from "react-hook-form";
+import { createTask } from "../taskSlice";
 type Inputs = {
   taskTitle: string;
 };
 
 const TaskForm: React.FC = () => {
+  //useDispatch()を使うためにdispatchを定義
+  const dispatch = useDispatch();
   const { register, handleSubmit, reset } = useForm();
   const handleCreate = (data: Inputs) => {
-    console.log(data);
+    //storeに定義したcreateTaskを引数として、createTaskが発火
+    //createTaskの引数にdata.taskTitleを指定すると、title:action.payloadに渡される。
+    //state.tasksが更新され、新しいstateが定義される
+    dispatch(createTask(data.taskTitle));
     reset();
   };
   return (
